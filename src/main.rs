57710b8cadd5a0fce270ff1d65073f6af5f5821e
HagbardCeline <janpauldahlke@gmail.com>
2022-12-02 08:56:07 +0100

    adding testcases

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    //answer_one()
 }
+fn read_input() {
+    fs::read_to_string("./inputs/day_1.txt").unwrap();
+}
+
+// region --_Day1
+fn answer_one(input: &str) -> String {
+    println!("{}", input);
+    "24000".to_string()
+}
+
+fn answer_two(input: &str) -> String {
+    println!("{}", input);
+    "24000".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn works() {
+        let test_input = "1000
+        2000
+        3000
+        ";
+
+        assert_eq!(answer_one(test_input), "24000")
+    }
+}
+
+// endregion -- Day1
