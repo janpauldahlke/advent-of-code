c6eb17dca4c0e9096cca954af0f39d14ec82fd0b
HagbardCeline <janpauldahlke@gmail.com>
2022-12-02 10:25:34 +0100

    day1

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
 use std::fs;
 
 fn main() {
-    //answer_one()
+    let inp = read_input();
+    //let out = answer_one(inp);
+    let out = answer_two(inp);
+    println!("{out}")
 }
-fn read_input() {
-    fs::read_to_string("./inputs/day_1.txt").unwrap();
+fn read_input() -> String {
+    let res = fs::read_to_string("./inputs/day_1.txt").unwrap();
+    res
 }
 
 // region --_Day1
-fn answer_one(input: &str) -> String {
-    println!("{}", input);
-    "24000".to_string()
+fn answer_one(input: String) -> String {
+    let one_result = input
+        .split("\n\n")
+        .map(|weight| {
+            weight
+                .lines()
+                .map(|item| item.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .max()
+        .unwrap();
+
+    one_result.to_string()
 }
 
-fn answer_two(input: &str) -> String {
-    println!("{}", input);
-    "24000".to_string()
+fn answer_two(input: String) -> String {
+    let mut two_result = input
+        .split("\n\n")
+        .map(|weight| {
+            weight
+                .lines()
+                .map(|item| item.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect::<Vec<_>>();
+
+    two_result.sort_by(|a, b| b.cmp(a));
+    let sum: u32 = two_result.iter().take(3).sum();
+
+    sum.to_string()
 }
 
 #[cfg(test)]
@@ -24,10 +50,7 @@ mod tests {
 
     #[test]
     fn works() {
-        let test_input = "1000
-        2000
-        3000
-        ";
+        let test_input = read_input();
 
         assert_eq!(answer_one(test_input), "24000")
     }
